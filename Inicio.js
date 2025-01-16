63499f673e37ac2f2c14722bfe3d7536813314f8
Eduardo-Goncalvess <edusilvarocha@hotmail.com>
2025-01-16 13:51:37 -0300

    Add files via upload

---
+++ Inicio.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+    if (window.innerWidth < 768) {
+        window.location.href = "https://eduardo-goncalvess.github.io/EcoIrriga/TS.html";
+    }
+});
+
+const menuToggle = document.getElementById('menu-toggle');
+const submenu = document.getElementById('submenu');
+
+menuToggle.addEventListener('click', () => {
+    submenu.classList.toggle('show');
+});
